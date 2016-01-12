37ed70ac8306f30611e5017c4e36f18a2dac8564
OskarKlintrot <oskar.klintrot@gmail.com>
2016-01-12 15:53:24 +0100

    Added bootstrap to upup and fetching data over https

--- src/app/scripts/redirectToHTTPS.js
+++ src/app/scripts/redirectToHTTPS.js
@@ -1,4 +1,4 @@
-var host = "github.com/OskarKlintrot/React-React-Router-Redux-and-Webpack-boilerplate" // Change to your own url
+var host = "oskarklintrotskolarbetewp14.github.io/1dv449_project"
 
 if ((host == window.location.host) && (window.location.protocol != "https:"))
     window.location.protocol = "https"
